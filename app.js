6a28d718cd86fd6f88d3a90d69120ef509dae686
Hamza Aziane <hamza.aziane@pm.me>
2022-06-27 22:29:10 +0100

    Select DOM elements

---
+++ app.js
@@ -0,0 +1,9 @@
+// Select DOM Elements
+const rangeSlider = document.querySelector("input#slider");
+const sliderLabel = document.querySelector(".range-slider label");
+const grid = document.querySelector('.grid');
+const clearBtn = document.querySelector(".clear");
+const eraserBtn = document.querySelector(".eraser");
+const ranModeBtn = document.querySelector(".rainbow-mode");
+const colorModeBtn = document.querySelector(".color-mode");
+const colorPicker = document.querySelector("input#color-picker");
