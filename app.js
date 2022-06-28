6eeda685fc47f5f3c1bb8d3bee662b2826ec7c9f
Hamza Aziane <hamza.aziane@pm.me>
2022-06-28 01:14:18 +0100

    Add grid's layout generator function

--- app.js
+++ app.js
@@ -11,4 +11,44 @@ const colorPicker = document.querySelector("input#color-picker");
 // Extract values from DOM elements
 let gridDimension = parseInt(rangeSlider.value);
 let penColor = colorPicker.value;
-let gridSize = parseInt(getComputedStyle(grid).width);
+const gridSize = parseInt(getComputedStyle(grid).width);
+
+// Grid layout generator function
+function generateGridLayout(gridDimension) {
+    // Calculate the square size based on the grid size
+    let squareSize = gridSize / gridDimension;
+    // Create the rows div
+    let rows = document.createElement('div');
+    rows.classList.add('rows');
+    // Style the rows
+    rows.style.display = 'flex';
+    rows.style.flexDirection = 'column';
+    rows.style.gap = '1px';
+    rows.style.height = '100%';
+    // A for loop to generate the grid
+    for (let i = 0; i < gridDimension; i++) {
+        // Create the row div
+        let row = document.createElement('div');
+        row.classList.add(`row-${i+1}`);
+        // Style the row
+        row.style.display = 'flex';
+        row.style.flexGrow = '1';
+        row.style.gap = '1px';
+        // A for loop to generate squares for each row
+        for (let j = 0; j < gridDimension; j++) {
+            // Create the square div
+            let square = document.createElement('div');
+            square.classList.add('square');
+            // Style the square
+            square.style.width = squareSize + 'px';
+            square.style.height = '100%';
+            square.style.background = '#D8D8D8';
+            // Append the square to the row
+            row.appendChild(square);
+        }
+        // Append the generated row to the list of rows
+        rows.appendChild(row);
+    }
+    // Append the rows list to the grid
+    grid.appendChild(rows);
+}
